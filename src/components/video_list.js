d7788b6d0e6527a4c3d9060ac31a20f18c7f86b9
Yonatan Arbel <yonatanarbel@Yonatans-MacBook-Air.local>
2017-05-16 01:47:59 +0300

    added iamges

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import VideoListItem from './video_list_item'
-const VideoList = (props)=>{
-const VideoItems =  props.videos.map((video)=>{
+const VideoList = (props) => {
+  const VideoItems = props.videos.map((video) => {
 
-return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video}/>
 
-});
-return(
-<ul className="col-md-4 list-group">
-{VideoItems}
-</ul>
+  });
+  return (
+    <ul className="col-md-4 list-group">
+      {VideoItems}
+    </ul>
 
-);
+  );
 
 };
 
